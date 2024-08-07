f034a7fbd31b4fd8e4aabbbde21b70640ce1576f
saradir <[superpostioned+github@gmail.com]>
2024-08-08 01:31:51 +0300

    fix most bugs

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let displayValue = '0';
-let operandA = null;
-let operandB = null;
+let a = null;
+let b = null;
 let operator = null;
+let state = "continue";
+let operatorState = "unlocked"
+let displayState = "clear";
 
 function add(a, b){
     return a + b;
@@ -12,63 +15,130 @@ function subtract(a, b){
 }
 
 function multiply(a, b){
-    return a * b
+    return a * b;
 }
 
 function divide(a, b){
-    return a / b
+    return a / b;
 }
 
 function operate(operator, a, b){
+    let result;
+    switch(operator){
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+    }
+
+    return result;
 }
 
 function updateDisplay(input){
-    display.textContent = input;
+    if(input){
+        display.textContent = input.toString();}
+    else {display.textContent = "0";};
+    
 }
 
 function handleInput(e){
 
     /* update display when digits clicked */
+
     if(e.target.classList.contains("digit")){
-        if(displayValue === "0"){
+        if(state === "prereset"){
+            reset();
+        }
+        if(displayState === "clear"){
             displayValue = '';
+            displayState = 'show';
+        }
+
+        if(operatorState === "locked"){
+            operatorState = "unlocked";
         }
         displayValue += e.target.value;
         updateDisplay(displayValue);
-    }
 
-    else if(e.target.classList.contains("operation")){
+    } else if(e.target.classList.contains("operation")){
 
-        if(!operator){
-            /* store display value as first operand (and second operand as default) */
-            operandA = operandB = Number(displayValue);
-            operator = e.target.value;
-            /* reset display value, but don't update display yet, in wait for second operand */
-            displayValue = ''
+        if(state === "prereset"){
+            state = "continue";
         }
-    }
 
-    else if(e.target.class="equals"){
-        operandB = Number(displayValue);
-        calculate();
-    }
+        /* allows only to change operator */
+        if(operatorState === "locked"){
+            operator = e.target.value;  
+            return;
+        }
 
-    else if(e.target.classList.contains("ac")){
+        operatorState = "locked";
+
+        storeValue();
+
+        if(a && b){
+            getResult();
+            storeValue();
+        }
+        operator = e.target.value;
+
+    } else if(e.target.id === "equals"){
+        if(operatorState === "locked"){
+            return;
+        }
+        if(state === "prereset"){
+            return;
+        }
+        storeValue();
+        if(a && b){
+            getResult();
+        }
+        state = "prereset";
+    } else if(e.target.classList.contains("ac")){
         reset();
+    } else if (e.target.classList.contains("backspace")){
+        deleteLastDigit();
     }
 }
 
-function calculate(){
+function getResult(){
+    displayValue = operate(operator, a, b);
+    updateDisplay(displayValue);
+    operator = null;
+    a = null;
+    b = null;
 }
 
 function reset(){
-    operandA = 0;
-    operandB = 0;
+    a = null;
+    b = null;
     operator = null;
-    displayValue = '0'
+    displayValue = '0';
+    displayState = "clear";
     updateDisplay(displayValue);
 }
 
+function deleteLastDigit(){
+    displayValue = displayValue.toString().slice(0,-1);
+    updateDisplay(displayValue);
+}
+
+function storeValue(){
+    if(!a){
+        a = Number(displayValue);
+    } else if(!b) {
+        b = Number(displayValue);
+    }
+    displayState = 'clear';  // clear display for next input
+}
 const display = document.querySelector(".display");
 const buttons = document.querySelector(".button-box");
 buttons.addEventListener("click", (e) => handleInput(e));
